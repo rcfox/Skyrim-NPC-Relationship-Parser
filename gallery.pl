7c9dc58cbdd606ce5836360364a34cb1ef1ced5d
Ryan Fox <foxryan@gmail.com>
2012-02-08 22:06:50 -0500

    Make the gallery script output some useful information in addition to the graphs.

--- gallery.pl
+++ gallery.pl
@@ -6,6 +6,7 @@ use warnings;
 my @images = glob("noname*.svg");
 
 print<<HTML_HEADER;
+<!DOCTYPE html>
 <html>
     <head>
         <title>
@@ -16,13 +17,31 @@ print<<HTML_HEADER;
         </style>
     </head>
     <body>
+        <h1>Skyrim NPC Relationship Graphs</h1>
         <p>
             Click an image to see the full-sized version. Or, get everything in <a href="relationships.svg">one, searchable image</a>.
         </p>
+        <p>
+            <a href="https://github.com/rcfox/Skyrim-NPC-Relationship-Parser">Get the code on GitHub!</a>
+        </p>
+        <div style="float: left;">
+            <h2>Affection level colour key</h2>
+            <ul>
+              <li><span style="color: forestgreen;">Lover</span></li>
+              <li><span style="color: green;">Ally</span></li>
+              <li><span style="color: lime;">Confidant</span></li>
+              <li><span style="color: olive;">Friend</span></li>
+              <li><span style="color: orange;">Acquaintance</span></li>
+              <li><span style="color: orangered;">Rival</span></li>
+              <li><span style="color: firebrick;">Foe</span></li>
+              <li><span style="color: red;">Enemy</span></li>
+              <li><span style="color: darkred;">Archnemesis</span></li>
+            </ul>
+        </div>
 HTML_HEADER
 
 for(@images) {
-	print<<LINK;
+    print<<LINK;
         <a href="$_"><img src="$_" alt="$_" /></a>
 LINK
 }
