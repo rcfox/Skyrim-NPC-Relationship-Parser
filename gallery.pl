6fdc74efe4245aa1857088859455f0cea65fb8ad
Ryan Fox <foxryan@gmail.com>
2012-03-18 19:53:22 -0400

    Add images for all of the characters.

--- gallery.pl
+++ gallery.pl
@@ -2,51 +2,53 @@
 use 5.010_000;
 use strict;
 use warnings;
+use File::Slurp qw(slurp);
 
 my @images = glob("noname*.svg");
 
 print<<HTML_HEADER;
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>
-            Skyrim NPC Relationships
-        </title>
-        <style>
-            img { max-width:100%; height: auto; }
-        </style>
-    </head>
-    <body>
-        <h1>Skyrim NPC Relationship Graphs</h1>
-        <p>
-            Click an image to see the full-sized version. Or, get everything in <a href="relationships.svg">one, searchable image</a>.
-        </p>
-        <p>
-            <a href="https://github.com/rcfox/Skyrim-NPC-Relationship-Parser">Get the code on GitHub!</a>
-        </p>
-        <div style="float: left;">
-            <h2>Affection level colour key</h2>
-            <ul>
-              <li><span style="color: forestgreen;">Lover</span></li>
-              <li><span style="color: green;">Ally</span></li>
-              <li><span style="color: lime;">Confidant</span></li>
-              <li><span style="color: olive;">Friend</span></li>
-              <li><span style="color: orange;">Acquaintance</span></li>
-              <li><span style="color: orangered;">Rival</span></li>
-              <li><span style="color: firebrick;">Foe</span></li>
-              <li><span style="color: red;">Enemy</span></li>
-              <li><span style="color: darkred;">Archnemesis</span></li>
-            </ul>
-        </div>
+	<head>
+		<title>
+			Skyrim NPC Relationships
+		</title>
+		<style>
+			img { max-width:100%; height: auto; }
+		</style>
+	</head>
+	<body>
+		<h1>Skyrim NPC Relationship Graphs</h1>
+		<p>
+			Click an image or name to go to that character's page on the <a href="http://www.uesp.net/">Unofficial Elder Scrolls Pages Wiki</a>. This is also available as <a href="relationships.svg">one, large image</a>.
+		</p>
+		<p>Your browser's "find" feature should also work to find characters. Some browsers (Chrome) will get you in the right area, but you generally have to scroll up for some reason.</p>
+		<p>All character images come from the characters' respective page on the <a href="http://www.uesp.net/">UESPWiki</a>.</p>
+		<p>Note: If a character's image is black, that means there was no standard-sized image availabe. If the image is white, then it didn't render correctly, and I don't know why...</p>
+		<p>
+			<a href="https://github.com/rcfox/Skyrim-NPC-Relationship-Parser">Get the code on GitHub!</a>
+		</p>
+		<div style="float: left;">
+			<h2>Affection level colour key</h2>
+			<ul>
+			  <li><span style="color: forestgreen;">Lover</span></li>
+			  <li><span style="color: green;">Ally</span></li>
+			  <li><span style="color: lime;">Confidant</span></li>
+			  <li><span style="color: olive;">Friend</span></li>
+			  <li><span style="color: orange;">Acquaintance</span></li>
+			  <li><span style="color: orangered;">Rival</span></li>
+			  <li><span style="color: firebrick;">Foe</span></li>
+			  <li><span style="color: red;">Enemy</span></li>
+			  <li><span style="color: darkred;">Archnemesis</span></li>
+			</ul>
+		</div>
 HTML_HEADER
 
 for(@images) {
-    print<<LINK;
-        <a href="$_"><img src="$_" alt="$_" /></a>
-LINK
+	print slurp($_);
 }
 
 print<<HTML_FOOTER;
-    </body>
+	</body>
 </html>
 HTML_FOOTER
--- relationships.pl
+++ relationships.pl
@@ -3,6 +3,8 @@ use 5.010_000;
 use strict;
 use warnings;
 use File::Slurp;
+use Digest::MD5 qw(md5_hex);
+use URI::Escape;
 
 my @names_file = read_file($ARGV[0]);
 my @relationships_file = read_file($ARGV[1]);
@@ -11,14 +13,14 @@ my %names;
 my %relationships;
 
 my %relationship_colours = (Lover => 'forestgreen',
-                            Ally => 'green',
-                            Confidant => 'lime',
-                            Friend => 'olive',
-                            Acquaintance => 'orange',
-                            Rival => 'orangered',
-                            Foe => 'firebrick',
-                            Enemy => 'red',
-                            Archnemesis => 'darkred');
+							Ally => 'green',
+							Confidant => 'lime',
+							Friend => 'olive',
+							Acquaintance => 'orange',
+							Rival => 'orangered',
+							Foe => 'firebrick',
+							Enemy => 'red',
+							Archnemesis => 'darkred');
 
 for(@names_file) {
 	if(/FormID: (.*?)\t\s*(.*?)\t\s*(.*?)\t\s*(.*)/) {
@@ -37,6 +39,7 @@ for(@relationships_file) {
 	}
 }
 
+my %node_printed;
 print "digraph Relationships {\n";
 
 for my $relid (keys %relationships) {
@@ -44,13 +47,18 @@ for my $relid (keys %relationships) {
 	my $alias2 = $relationships{$relid}{alias2};
 	my $name1 = $names{$alias1}{name};
 	my $name2 = $names{$alias2}{name};
+	my $image1 = get_image($name1);
+	my $image2 = get_image($name2);
 	my $type = $relationships{$relid}{type};
 	my $level = $relationships{$relid}{level};
 	my $colour = $relationship_colours{$level};
 	my $url = "http://www.uesp.net/wiki/Skyrim:";
 
-	print "$alias1 [label=\"$name1\" URL=\"$url$name1\"];\n";
-	print "$alias2 [label=\"$name2\" URL=\"$url$name2\"];\n";
+	print "$alias1 [shape=none label=<<TABLE BORDER=\"0\"><TR><TD><IMG SRC=\"$image1\"/></TD></TR><TR><TD>$name1</TD></TR></TABLE>> URL=\"$url$name1\"];\n" unless($node_printed{$name1});
+	print "$alias2 [shape=none label=<<TABLE BORDER=\"0\"><TR><TD><IMG SRC=\"$image2\"/></TD></TR><TR><TD>$name2</TD></TR></TABLE>> URL=\"$url$name2\"];\n" unless($node_printed{$name2});
+
+	$node_printed{$name1} = 1;
+	$node_printed{$name2} = 1;
 
 	my $edge = "[label=\"$type\" color=\"$colour\" edgetooltip=\"$name1 -> $name2 ($type, $level)\"]";
 	$edge = "[color=\"$colour\" edgetooltip=\"$name1 -> $name2 ($level)\"]" if ($type ~~ "NONE");
@@ -59,3 +67,21 @@ for my $relid (keys %relationships) {
 }
 
 print "}\n";
+
+
+sub get_image
+{
+	my $name = shift;
+	my $filename = "SR-npc-$name.jpg";
+	$filename =~ s/\s/_/g;
+	my $e_filename = uri_escape($filename);
+	my $sized_filename = "120px-$filename";
+	my $sized_e_filename = "120px-$e_filename";
+	return $sized_filename if(-e $sized_filename);
+
+	my $md5 = md5_hex($filename);
+	my $url = "http://images.uesp.net/thumb/".substr($md5,0,1)."/".substr($md5,0,2)."/$e_filename/$sized_filename";
+	#`wget -nc $url`;
+	return $sized_filename if(-e $sized_filename);
+	return "dummy.jpg";
+}
